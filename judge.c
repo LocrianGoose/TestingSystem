e453138476c532053caa82963924131ade13e3e6
LocrianGoose <azairLocr@protonmail.ch>
2018-11-18 16:31:06 +0600

    making function for launch test

--- judge.c
+++ judge.c
@@ -13,26 +13,37 @@
 
 
 typedef struct {
-	DIR *dir;
 	char *name;
+	char **solutions;
 	int points;
 } Participant;
+
 
 void s_chdir(char *path);
 int s_open(char *fileName, int flag);
 void *s_realloc(void *ptr, int size);
 
-void compile(char *name)
+char **get_solutions(DIR *sol_dir)
 {
-	pid_t pid = fork();
-	if (pid < 0) {
-		perror("fork failed");
-		exit(-1);
-	} else if (pid) {
-		wait(NULL);
-	} else {
-		execlp("make", "make", name, NULL);
+	char **solutions = NULL;
+	char *tmp;
+	struct dirent *entry;
+	int i = 0, max_len = 1;
+
+	while ((entry = readdir(sol_dir)) != NULL) {
+		if (i + 1 >= max_len) {
+			max_len <<= 1;
+			solutions = s_realloc(solutions,
+					max_len * sizeof(char *));
+		}
+		tmp = entry->d_name;
+		if (strncmp(tmp, ".", 2) && strncmp(tmp, "..", 3)) //add filter
+			solutions[i++] = tmp;
 	}
+	if (i == 0)
+		free(solutions);
+	solutions[i] = NULL;
+	return solutions;
 }
 
 Participant *get_participants_list(void)
@@ -40,7 +51,7 @@ Participant *get_participants_list(void)
 	int i = 0, max_len = 1;
 	struct dirent *entry;
 	char *tmp;
-	DIR *participants_dir = opendir("participants");
+	DIR *participants_dir = opendir("participants"), *sol_dir;
 	Participant *participants = NULL;
 
 	if (participants_dir == NULL) {
@@ -54,11 +65,13 @@ Participant *get_participants_list(void)
 			participants = s_realloc(participants,
 					max_len * sizeof(Participant));
 		}
-		participants[i].dir = opendir(entry->d_name);
-		if (participants[i].dir != NULL) {
+		sol_dir = opendir(entry->d_name);
+		if (sol_dir != NULL) {
 			participants[i].name = (tmp = entry->d_name);
-			participants[i].points = 0;
 			if (strncmp(tmp, ".", 2) && strncmp(tmp, "..", 3)) {
+				participants[i].points = 0;
+				participants[i].solutions =
+					get_solutions(sol_dir);
 				i++;
 			}
 		} else {
@@ -75,6 +88,30 @@ Participant *get_participants_list(void)
 	closedir(participants_dir);
 	return participants;
 }
+
+void testing(Participant *prts, char *contest)
+{
+	pid_t pid;
+
+	for (int i = 0; prts[i].name != NULL; i++) {
+		for (int j = 0; prts[i].solutions[j] != NULL; j++) {
+			pid = fork();
+			if (pid < 0) {
+				perror("fork failed");
+			} else if (pid == 0) {
+				//getresult
+			} else {
+				if (execlp("tester", "tester",
+						contest, prts[i].name,
+						prts[i].solutions[j],
+						NULL) < 0) {
+					perror("Tried to exec tester, but failed");
+				}
+			}
+		}
+	}
+}
+
 
 void init(int argc, char **argv, int *config)
 {
@@ -97,6 +134,7 @@ int main(int argc, char **argv)
 
 	init(argc, argv, &config);
 	participants = get_participants_list();
+	//testing(participants, argv[1]);
 	free(participants);
 	close(config);
 	return 0;
